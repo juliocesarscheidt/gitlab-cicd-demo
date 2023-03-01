6a67d8d6be3f9b18790df8eacc2382e7574632f8
juliocesarscheidt <julio.scheidt96@gmail.com>
2023-03-01 14:02:30 -0300

    Changes

--- environments/README.md
+++ environments/README.md
@@ -1,24 +1,26 @@
+> https://docs.gitlab.com/ee/user/infrastructure/iac/terraform_state.html
+
+> https://docs.gitlab.com/ee/user/infrastructure/iac/mr_integration.html
 
 ```bash
 export PROJECT_ID=""
-export TF_HTTP_USERNAME=""
-export TF_HTTP_PASSWORD=""
-export TF_HTTP_ADDRESS="https://gitlab.com/api/v4/projects/${PROJECT_ID}/terraform/state/state"
-export TF_HTTP_RETRY_WAIT_MIN="1"
+export TF_USERNAME=""
+export TF_PASSWORD=""
+export TF_ADDRESS="https://gitlab.com/api/v4/projects/${PROJECT_ID}/terraform/state/state"
 
 export AWS_REGION="us-east-1"
 export TF_VAR_domain_name="bitcoin-bucket-web-$AWS_REGION"
 export TF_VAR_bucket_name="bitcoin-bucket-web-$AWS_REGION"
 
 terraform init \
-  -backend-config=address=${TF_HTTP_ADDRESS} \
-  -backend-config=lock_address=${TF_HTTP_ADDRESS}/lock \
-  -backend-config=unlock_address=${TF_HTTP_ADDRESS}/lock \
-  -backend-config=username=${TF_HTTP_USERNAME} \
-  -backend-config=password=${TF_HTTP_PASSWORD} \
+  -backend-config=address=${TF_ADDRESS} \
+  -backend-config=lock_address=${TF_ADDRESS}/lock \
+  -backend-config=unlock_address=${TF_ADDRESS}/lock \
+  -backend-config=username=${TF_USERNAME} \
+  -backend-config=password=${TF_PASSWORD} \
   -backend-config=lock_method=POST \
   -backend-config=unlock_method=DELETE \
-  -backend-config=retry_wait_min=${TF_HTTP_RETRY_WAIT_MIN}
+  -backend-config=retry_wait_min=5
 
 terraform validate
 
--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,13 +1,11 @@
 terraform {
   required_version = ">= 1.2"
-
   required_providers {
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
       version = "=4.55.0"
     }
   }
-
   backend "http" {
   }
 }
@@ -34,8 +32,12 @@ variable "bucket_name" {
 
 resource "aws_s3_bucket" "web_bucket" {
   bucket = var.bucket_name
+  tags   = {}
+}
+
+resource "aws_s3_bucket_policy" "web_bucket_policy" {
+  bucket = aws_s3_bucket.web_bucket.id
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-  tags = {}
 }
 
 resource "aws_s3_bucket_cors_configuration" "web_bucket_cors_configuration" {
@@ -63,18 +65,33 @@ resource "aws_s3_bucket_website_configuration" "web_bucket_website_configuration
   }
 }
 
+resource "aws_s3_bucket_versioning" "web_bucket_versioning" {
+  bucket = aws_s3_bucket.web_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_object" "web_index" {
-  bucket       = aws_s3_bucket.web_bucket.id
-  key          = "index.html"
-  content_type = "text/html"
-  source       = "../../src/index.html"
-  acl          = "public-read"
+  bucket        = aws_s3_bucket.web_bucket.id
+  key           = "index.html"
+  content_type  = "text/html"
+  source        = "../../src/index.html"
+  etag          = filemd5("../../src/index.html")
+  acl           = "public-read"
+  force_destroy = true
 }
 
 resource "aws_s3_object" "web_404" {
-  bucket       = aws_s3_bucket.web_bucket.id
-  key          = "404.html"
-  content_type = "text/html"
-  source       = "../../src/404.html"
-  acl          = "public-read"
+  bucket        = aws_s3_bucket.web_bucket.id
+  key           = "404.html"
+  content_type  = "text/html"
+  source        = "../../src/404.html"
+  etag          = filemd5("../../src/404.html")
+  acl           = "public-read"
+  force_destroy = true
+}
+
+output "website_endpoint" {
+  value = "http://${aws_s3_bucket_website_configuration.web_bucket_website_configuration.website_endpoint}"
 }
