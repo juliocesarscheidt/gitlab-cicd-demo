16a80a1dd85e1c51ae5f0fa55ef951e15f0d48cd
juliocesarscheidt <julio.scheidt96@gmail.com>
2024-02-01 13:01:11 -0300

    Improvements

--- job/main.js
+++ job/main.js
@@ -2,14 +2,12 @@ const AWS = require('aws-sdk');
 const moment = require('moment');
 const { MongoClient } = require('mongodb');
 
-const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/?maxPoolSize=1';
+const mongoUri = process.env.MONGO_URI;
 const mongoDatabase = process.env.MONGO_DATABASE || 'bitcoin';
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.AWS_REGION || 'us-east-1';
 const s3BucketName = process.env.S3_BUCKET_NAME;
 
-AWS.config.update({ accessKeyId, secretAccessKey, region });
+AWS.config.update({ region });
 
 const persistCurrency = async (mongoClient, database, doc) => {
   const db = mongoClient.db(database);
@@ -26,7 +24,7 @@ const retrieveS3ObjectByDate = async (s3Client, s3BucketName, today) => {
   const year = today.format('YYYY');
   const month = today.format('MM');
   const day = today.format('DD');
-  const s3Key = `bitcoin/year=${year}/month=${month}/day=${day}/last.json`;
+  const s3Key = `bitcoin/year=${year}/month=${month}/day=${day}/last`;
   const s3GetParams = {
     Bucket: s3BucketName,
     Key: s3Key,
@@ -43,6 +41,7 @@ const retrieveS3ObjectByDate = async (s3Client, s3BucketName, today) => {
 }
 
 (async () => {
+  console.info('[INFO] mongoUri', mongoUri);
   const mongoClient = new MongoClient(mongoUri);
   await mongoClient.connect();
   const s3Client = new AWS.S3({ apiVersion: '2013-04-01' });
